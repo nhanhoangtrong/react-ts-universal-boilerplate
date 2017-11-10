2c4730bc99f4e91faac59a6a5c55ad45b2bb92f5
Nhan Hoang <siege250294@gmail.com>
2017-11-10 18:25:53 +0700

    yarn: adding React 16 packages

--- src/client.prod.tsx
+++ src/client.prod.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { render } from 'react-dom';
+import { hydrate } from 'react-dom';
 import App from './components/App';
 import { configureStore, apolloClient } from './redux';
 
@@ -8,4 +8,4 @@ import { createBrowserHistory } from 'history';
 const history = createBrowserHistory();
 const store = configureStore(history, window.__PRELOADEDSTATE__);
 
-render(<App store={store} history={history} client={apolloClient}/>, document.getElementById('app'));
+hydrate(<App store={store} history={history} client={apolloClient}/>, document.getElementById('app'));
